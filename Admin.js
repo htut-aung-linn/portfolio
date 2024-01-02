b27d2aab200a734a03fd704d0b069275e712a02e
Htut Aung Ling <htutlinn686@gmail.com>
2024-01-01 22:26:30 -0800

    complete main frame of admin page

--- Admin.js
+++ Admin.js
@@ -1,7 +1,10 @@
+//ALTER TABLE `message` ADD `No` INT NOT NULL AUTO_INCREMENT AFTER `State`, ADD PRIMARY KEY (`No`);
 //elements
-const popup = document.getElementById('popup');
-
+var popup = document.getElementById('popup');
+//get all message from database
+var getstate = "Unseen";
 function getall(){
+    getstate = "All";
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -9,27 +12,153 @@ function getall(){
         const a = JSON.parse(xhttp.responseText);
         var text = "";
         for(var i = 0;  i< a.length; i++){
-            console.log(a[i][0][0]);
-            text +=array_to_message(a[i][0]);
+            console.log(a[i][0]);
+            text +=array_to_message(a[i]);
         }
         }
         console.log(typeof(text));
-        document.getElementById('popup').innerHTML = text;
+        document.getElementById('container').innerHTML = text;
     };
     xhttp.open("GET", "adminReq.php?f=all", true);
     xhttp.send(); 
 }
+//getting unseen message from database
+function getunseen(){
+    getstate = "Unseen";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        const a = JSON.parse(xhttp.responseText);
+        var text = "";
+        for(var i = 0;  i< a.length; i++){
+            console.log(a[i][0]);
+            text +=array_to_message(a[i]);
+        }
+        }
+        console.log(typeof(text));
+        document.getElementById('container').innerHTML = text;
+    };
+    xhttp.open("GET", "adminReq.php?f=us", true);
+    xhttp.send(); 
+}
+//geting finished messages
+function getdone(){
+    getstate = "Done";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        const a = JSON.parse(xhttp.responseText);
+        var text = "";
+        for(var i = 0;  i< a.length; i++){
+            console.log(a[i][0]);
+            text +=array_to_message(a[i]);
+        }
+        }
+        console.log(typeof(text));
+        document.getElementById('container').innerHTML = text;
+    };
+    xhttp.open("GET", "adminReq.php?f=d", true);
+    xhttp.send();
+}
 
+//get in progress message
+function getprogress(){
+    getstate = "In Progress";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        const a = JSON.parse(xhttp.responseText);
+        var text = "";
+        for(var i = 0;  i< a.length; i++){
+            console.log(a[i][0]);
+            text +=array_to_message(a[i]);
+        }
+        }
+        console.log(typeof(text));
+        document.getElementById('container').innerHTML = text;
+    };
+    xhttp.open("GET", "adminReq.php?f=pro", true);
+    xhttp.send();
+}
 function array_to_message(array){
-    const string = '<div class="message">'+
+    const state = array[3];
+    var btn = "";
+    if(state==="P"){
+        btn = '<button onclick="don('+array[4]+')">Done</button>'
+    } else{
+        btn = '<button onclick="per('+array[4]+')">Perform</button>';
+    }
+    const string = '<div class="message '+array[3]+'">'+
     '<h3>Name: '+array[0]+'</h3>'+
     '<p>Message: '+array[2]+'</p>'+
     '<a>To Contact: '+array[1]+'</a>'+
-   ' <div class="button">'+
-        '<button onclick="">Seen</button>'+
-        '<button>Done</button>'+
-        '<button>Delete</button>'+
+    '<div class="button">'+btn+
+        '<button onclick="deldo('+array[4]+')">Delete</button>'+
     '</div>'+
-'</div>';
+    '</div>';
     return string;
 }
+
+function per(no){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            refresh();
+        }
+    };
+    console.log("adminReq.php?f=per&no="+no);
+    xhttp.open("GET", "adminReq.php?f=per&no="+no, true);
+    xhttp.send(); 
+}
+function don(no){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            refresh();
+        }
+    };
+    console.log("adminReq.php?f=per&no="+no);
+    xhttp.open("GET", "adminReq.php?f=don&no="+no, true);
+    xhttp.send();
+}
+function cancle(){
+    document.getElementById('popup').style.display = "none";
+}
+function del(no){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('popup').style.display = "none";
+            refresh();
+        }
+    };
+    xhttp.open("GET", "adminReq.php?f=del&no="+no, true);
+    xhttp.send();
+}
+function deldo(no){
+    const text = '<div>'+
+    'Do you want to delete this message?'+
+'</div>'+
+'<div class="button">'+
+    '<button onclick="del('+no+')">Delete</button>'+
+    '<button onclick="cancle()">Cancle</button>'+
+'</div>';
+    document.getElementById("popup").innerHTML = text;
+    document.getElementById('popup').style.display = "block";
+}
+
+function refresh(){
+    if(getstate==="All"){
+        getall();
+    }else if(getstate==="Unseen"){
+        getunseen();
+    }else if(getstate==="Done"){
+        getdone();
+    }else{
+        getprogress();
+    }
+}
+//ALTER TABLE `message` ADD `No` INT NOT NULL AUTO_INCREMENT AFTER `State`, ADD PRIMARY KEY (`No`);
--- adminReq.php
+++ adminReq.php
@@ -1,17 +1,63 @@
 <?php
 //f = functions
-
+//us = unseen
 $fun = $_GET['f'];
 include("databaseConnection.php");
-if($fun = "all"){
+if($fun == "all"){
     $sql = "SELECT * FROM `message`";
     $data = array();
     if($result = mysqli_query($conn, $sql)){
         while($message =  mysqli_fetch_array($result)) {
-            $sd = array([$message[0], $message[1], $message[2], $message[3]]);
+            $sd = array($message[0], $message[1], $message[2], $message[3], $message[4]);
             array_push($data, $sd);
         }
     }
     echo json_encode($data);
-    mysqli_close($conn);
+}elseif($fun=='us'){
+    //SELECT * FROM `message` WHERE `State`='O';
+    $sql = "SELECT * FROM `message` WHERE `State`='O' OR `State`='S'";
+    $data = array();
+    if($result = mysqli_query($conn, $sql)){
+        while($message =  mysqli_fetch_array($result)) {
+            $sd = array($message[0], $message[1], $message[2], $message[3], $message[4]);
+            array_push($data, $sd);
+        }
+    }
+    echo json_encode($data);
+}elseif($fun=="d"){//d = done, finished messages
+    $sql = "SELECT * FROM `message` WHERE `State`='D'";
+    $data = array();
+    if($result = mysqli_query($conn, $sql)){
+        while($message =  mysqli_fetch_array($result)) {
+            $sd = array($message[0], $message[1], $message[2], $message[3], $message[4]);
+            array_push($data, $sd);
+        }
+    }
+    echo json_encode($data);
+}elseif($fun=="per"){
+    $no = $_GET['no'];
+    //UPDATE `message` SET `State`='P' WHERE `No`='1';
+    $sql = "UPDATE `message` SET `State`='P' WHERE `No`='$no'";
+    mysqli_query($conn, $sql);
+}elseif($fun=="don"){
+    $no = $_GET['no'];
+    //UPDATE `message` SET `State`='P' WHERE `No`='1';
+    $sql = "UPDATE `message` SET `State`='D' WHERE `No`='$no'";
+    mysqli_query($conn, $sql);
+}elseif($fun=="pro"){
+    $sql = "SELECT * FROM `message` WHERE `State`='P'";
+    $data = array();
+    if($result = mysqli_query($conn, $sql)){
+        while($message =  mysqli_fetch_array($result)) {
+            $sd = array($message[0], $message[1], $message[2], $message[3], $message[4]);
+            array_push($data, $sd);
+        }
+    }
+    echo json_encode($data);
+}elseif($fun=="del"){
+    $no = $_GET['no'];
+    $sql = "DELETE FROM `message` WHERE `No`='$no'";
+    mysqli_query($conn, $sql);
 }
+
+mysqli_close($conn);
